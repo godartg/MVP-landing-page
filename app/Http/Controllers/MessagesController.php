5063f31d4cb4c11ce904cee2d104a7ad61b640a4
Edwin Condori <sl.vilcapuma.edwin@gmail.com>
2019-02-13 09:35:58 -0500

    Creación de proyecto

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Message;
+use Carbon\Carbon;
+use App\Http\Requests;
+use Illuminate\Database\Eloquent\Model;
+use App\Http\Requests\CreateMessageRequest;
+
+class MessagesController extends Controller
+{
+    //
+}
